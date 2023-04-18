2bf6c5215bfd86e7428c0e3d84eb03f659c026a3
Eric Cavalcanti <cajuclc@gmail.com>
2023-04-17 22:36:26 -0500

    Add pull always to run.sh

--- run.sh
+++ run.sh
@@ -2,15 +2,14 @@
 source ./env.sh
 docker stop n8n
 docker rm n8n
-docker images rm n8n-puppeteer:$N8N_VERSION
 docker run -d -it \
     --restart always \
     --privileged \
     --shm-size=1gb \
-	--env NODE_FUNCTION_ALLOW_BUILTIN="*" \
 	--env NODE_FUNCTION_ALLOW_EXTERNAL="*" \
+	--pull=always \
 	--name n8n \
 	-p 5678:5678 \
 	-v ~/.n8n:/home/node/.n8n \
-	n8n-puppeteer:$N8N_VERSION \
+	ghcr.io/cajuclc/n8n-puppeteer-docker:$N8N_VERSION \
 	n8n start --tunnel
